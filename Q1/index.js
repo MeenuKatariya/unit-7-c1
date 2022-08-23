8071a2d0936fe4a9c9eb6b9a8c7ef5afec77b306
unknown <meenukatariya2330@gmail.com>
2022-08-22 21:28:23 -0700

    done

---
+++ Q1/index.js
@@ -0,0 +1,31 @@
+const dns = require('dns');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use((req,res,next)=>{
+    console.log(req.method, req.path);
+    next();
+}
+)
+app.post('/getmeip', (req, res) => {
+    let { website_name } = req.body;
+    dns.lookup(website_name, (err, address) => {
+        if (err) {
+            return res.status(404).send({
+                message: 'Website not available'
+            });
+        }
+        return res.status(200).send({
+            website_name: website_name,
+            ip_address: address
+        });
+    }
+    );
+});
+
+app.listen(7000, () => {
+    console.log('Server is running on port 7000');
+});
